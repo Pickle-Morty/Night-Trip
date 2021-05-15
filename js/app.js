69aa1b9c3146883046d669c1d4808db8700d4ed7
Pickle-Morty <avatarbliz@gmail.com>
2021-05-15 13:00:06 +0600

    upgrade

--- js/app.js
+++ js/app.js
@@ -1,120 +1,141 @@
 $(document).ready(function () {
   var owl = $('.owl-carousel');
   owl.owlCarousel({
-      margin: 10,
-      dots: false,
-      nav: true,
-      loop: true,
-      mouseDrag: false,
-      touchDrag: false,
-      animateOut: 'fadeOut',
-      autoplay: false,
-      lazyLoad: true,
-      lazyLoadEager: 1,
-      navText: [
-          '<svg width="14" height="20" aria-hidden="true" focusable="false" data-prefix="fas" data-icon="arrow-left" class="svg-inline--fa fa-arrow-left fa-w-14" role="img" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512"><path fill="currentColor" d="M257.5 445.1l-22.2 22.2c-9.4 9.4-24.6 9.4-33.9 0L7 273c-9.4-9.4-9.4-24.6 0-33.9L201.4 44.7c9.4-9.4 24.6-9.4 33.9 0l22.2 22.2c9.5 9.5 9.3 25-.4 34.3L136.6 216H424c13.3 0 24 10.7 24 24v32c0 13.3-10.7 24-24 24H136.6l120.5 114.8c9.8 9.3 10 24.8.4 34.3z"></path></svg>',
-          '<svg width="14" height="20" aria-hidden="true" focusable="false" data-prefix="fas" data-icon="arrow-right" class="svg-inline--fa fa-arrow-right fa-w-14" role="img" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512"><path fill="currentColor" d="M190.5 66.9l22.2-22.2c9.4-9.4 24.6-9.4 33.9 0L441 239c9.4 9.4 9.4 24.6 0 33.9L246.6 467.3c-9.4 9.4-24.6 9.4-33.9 0l-22.2-22.2c-9.5-9.5-9.3-25 .4-34.3L311.4 296H24c-13.3 0-24-10.7-24-24v-32c0-13.3 10.7-24 24-24h287.4L190.9 101.2c-9.8-9.3-10-24.8-.4-34.3z"></path></svg>'
-      ],
-      responsive: {
-          0: {
-              items: 1
-          },
-          600: {
-              items: 1
-          },
-          1000: {
-              items: 1
-          }
+    margin: 10,
+    dots: false,
+    nav: true,
+    loop: true,
+    mouseDrag: false,
+    touchDrag: false,
+    animateOut: 'fadeOut',
+    autoplay: false,
+    lazyLoad: true,
+    lazyLoadEager: 1,
+    navText: [
+      '<svg width="14" height="20" aria-hidden="true" focusable="false" data-prefix="fas" data-icon="arrow-left" class="svg-inline--fa fa-arrow-left fa-w-14" role="img" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512"><path fill="currentColor" d="M257.5 445.1l-22.2 22.2c-9.4 9.4-24.6 9.4-33.9 0L7 273c-9.4-9.4-9.4-24.6 0-33.9L201.4 44.7c9.4-9.4 24.6-9.4 33.9 0l22.2 22.2c9.5 9.5 9.3 25-.4 34.3L136.6 216H424c13.3 0 24 10.7 24 24v32c0 13.3-10.7 24-24 24H136.6l120.5 114.8c9.8 9.3 10 24.8.4 34.3z"></path></svg>',
+      '<svg width="14" height="20" aria-hidden="true" focusable="false" data-prefix="fas" data-icon="arrow-right" class="svg-inline--fa fa-arrow-right fa-w-14" role="img" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512"><path fill="currentColor" d="M190.5 66.9l22.2-22.2c9.4-9.4 24.6-9.4 33.9 0L441 239c9.4 9.4 9.4 24.6 0 33.9L246.6 467.3c-9.4 9.4-24.6 9.4-33.9 0l-22.2-22.2c-9.5-9.5-9.3-25 .4-34.3L311.4 296H24c-13.3 0-24-10.7-24-24v-32c0-13.3 10.7-24 24-24h287.4L190.9 101.2c-9.8-9.3-10-24.8-.4-34.3z"></path></svg>'
+    ],
+    responsive: {
+      0: {
+        items: 1
+      },
+      600: {
+        items: 1
+      },
+      1000: {
+        items: 1
       }
+    }
   })
 })
 
 
 
-
-
 var $tabs = function (target) {
-    var
-      _elemTabs = (typeof target === 'string' ? document.querySelector(target) : target),
-      _eventTabsShow,
-      _showTab = function (tabsLinkTarget) {
-        var tabsPaneTarget, tabsLinkActive, tabsPaneShow;
-        tabsPaneTarget = document.querySelector(tabsLinkTarget.getAttribute('href'));
-        tabsLinkActive = tabsLinkTarget.parentElement.querySelector('.tabs__link_active');
-        tabsPaneShow = tabsPaneTarget.parentElement.querySelector('.tabs__pane_show');
-        // если следующая вкладка равна активной, то завершаем работу
-        if (tabsLinkTarget === tabsLinkActive) {
-          return;
-        }
-        // удаляем классы у текущих активных элементов
-        if (tabsLinkActive !== null) {
-          tabsLinkActive.classList.remove('tabs__link_active');
-        }
-        if (tabsPaneShow !== null) {
-          tabsPaneShow.classList.remove('tabs__pane_show');
-        }
-        // добавляем классы к элементам (в завимости от выбранной вкладки)
-        tabsLinkTarget.classList.add('tabs__link_active');
-        tabsPaneTarget.classList.add('tabs__pane_show');
-        document.dispatchEvent(_eventTabsShow);
-      },
-      _switchTabTo = function (tabsLinkIndex) {
-        var tabsLinks = _elemTabs.querySelectorAll('.tabs__link');
-        if (tabsLinks.length > 0) {
-          if (tabsLinkIndex > tabsLinks.length) {
-            tabsLinkIndex = tabsLinks.length;
-          } else if (tabsLinkIndex < 1) {
-            tabsLinkIndex = 1;
-          }
-          _showTab(tabsLinks[tabsLinkIndex - 1]);
-        }
-      };
-
-    _eventTabsShow = new CustomEvent('tab.show', { detail: _elemTabs });
-
-    _elemTabs.addEventListener('click', function (e) {
-      var tabsLinkTarget = e.target;
-      // завершаем выполнение функции, если кликнули не по ссылке
-      if (!tabsLinkTarget.classList.contains('tabs__link')) {
+  var
+    _elemTabs = (typeof target === 'string' ? document.querySelector(target) : target),
+    _eventTabsShow,
+    _showTab = function (tabsLinkTarget) {
+      var tabsPaneTarget, tabsLinkActive, tabsPaneShow;
+      tabsPaneTarget = document.querySelector(tabsLinkTarget.getAttribute('href'));
+      tabsLinkActive = tabsLinkTarget.parentElement.querySelector('.tabs__link_active');
+      tabsPaneShow = tabsPaneTarget.parentElement.querySelector('.tabs__pane_show');
+      // если следующая вкладка равна активной, то завершаем работу
+      if (tabsLinkTarget === tabsLinkActive) {
         return;
       }
-      // отменяем стандартное действие
-      e.preventDefault();
-      _showTab(tabsLinkTarget);
-    });
-
-    return {
-      showTab: function (target) {
-        _showTab(target);
-      },
-      switchTabTo: function (index) {
-        _switchTabTo(index);
+      // удаляем классы у текущих активных элементов
+      if (tabsLinkActive !== null) {
+        tabsLinkActive.classList.remove('tabs__link_active');
       }
+      if (tabsPaneShow !== null) {
+        tabsPaneShow.classList.remove('tabs__pane_show');
+      }
+      // добавляем классы к элементам (в завимости от выбранной вкладки)
+      tabsLinkTarget.classList.add('tabs__link_active');
+      tabsPaneTarget.classList.add('tabs__pane_show');
+      document.dispatchEvent(_eventTabsShow);
+    },
+    _switchTabTo = function (tabsLinkIndex) {
+      var tabsLinks = _elemTabs.querySelectorAll('.tabs__link');
+      if (tabsLinks.length > 0) {
+        if (tabsLinkIndex > tabsLinks.length) {
+          tabsLinkIndex = tabsLinks.length;
+        } else if (tabsLinkIndex < 1) {
+          tabsLinkIndex = 1;
+        }
+        _showTab(tabsLinks[tabsLinkIndex - 1]);
+      }
+    };
+
+  _eventTabsShow = new CustomEvent('tab.show', { detail: _elemTabs });
+
+  _elemTabs.addEventListener('click', function (e) {
+    var tabsLinkTarget = e.target;
+    // завершаем выполнение функции, если кликнули не по ссылке
+    if (!tabsLinkTarget.classList.contains('tabs__link')) {
+      return;
     }
+    // отменяем стандартное действие
+    e.preventDefault();
+    _showTab(tabsLinkTarget);
+  });
 
-  };
+  return {
+    showTab: function (target) {
+      _showTab(target);
+    },
+    switchTabTo: function (index) {
+      _switchTabTo(index);
+    }
+  }
 
-  $tabs('.tabs');   
-
- 
-
-
-document.querySelector('#menu').onclick = () => {
-  console.log('Пуф')
-  document.querySelector('#menu').classList.toggle ('active');
-  document.querySelector('#nav').classList.toggle ('active');
-  document.querySelector('body').classList.toggle ('active');
+};
   
+$tabs('.tabs');
+
+
+
+menuBtn = document.querySelector('.header__btn')
+
+
+menuBtn.onclick = () => {
+  menuBtn.classList.toggle('active');
+  document.querySelector('#nav').classList.toggle('active');
+  document.querySelector('.header').classList.toggle('active');
+  document.querySelector('body').classList.toggle('active');
+  document.querySelector('.header__btn i').classList.toggle('fa-times');
 }
 
 var elements = document.querySelectorAll(".nav__link");
 for (var i = 0; i < elements.length; i++) {
-  elements[i].onclick = function(){
-    console.log('МУР')
-    document.querySelector('#menu').classList.remove ('active');
-    document.querySelector('#nav').classList.remove ('active');
-    document.querySelector('body').classList.remove ('active');
+  elements[i].onclick = function () {
+    menuBtn.classList.remove('active');
+    document.querySelector('#nav').classList.remove('active');
+    document.querySelector('body').classList.remove('active');
   };
 }
 
+let socialList = document.querySelectorAll('.social__mobile');
 
+for (let a = 0; a < socialList.length; a++) {
+
+  let bloomingMenu = new BloomingMenu({
+    startAngle: -270,
+    endAngle: -180,
+    radius: 100,
+    itemAnimationDelay: 0.05,
+    itemsNum: 3,
+    animationDuration: 1,
+    fatherElement: socialList[a]
+  })
+
+  bloomingMenu.render()
+  // Prevents "elastic scrolling" on Safari
+  document.addEventListener('touchmove', function (event) {
+    'use strict'
+    event.preventDefault()
+  })
+
+
+}
